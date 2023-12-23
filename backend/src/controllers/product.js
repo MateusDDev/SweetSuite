3b67b5a1e36418f3d33aa8dd6a4d2cf68939961b
MateusDDev <ms4811978@gmail.com>
2023-12-23 11:44:41 -0300

    crud

--- backend/src/controllers/product.js
+++ backend/src/controllers/product.js
@@ -2,6 +2,47 @@ const connection = require('../db/connection');
 
 const getProducts = () => connection.execute('SELECT * FROM products;');
 
+const getProductsById = (id) => connection.execute('SELECT * FROM products WHERE id = ?', [id]);
+
+const addProduct = (product) => {
+  const query = `INSERT INTO products (name, description, price, quantity)
+                  VALUES (?, ?, ?, ?);`;
+
+  const values = [
+    product.name, 
+    product.description, 
+    product.price, 
+    product.quantity,
+  ];
+
+  return connection.execute(query, values);
+};
+
+const updateProduct = (product, id) => {
+  const query = `UPDATE products
+                  SET name = ?, description = ?, price = ?, quantity = ?
+                WHERE id = ?;`;
+
+  const values = [
+    product.name,
+    product.description,
+    product.price,
+    product.quantity,
+  ];
+
+  connection.execute(query, [...values, id]);
+};
+
+const removeProduct = (id) => {
+  const query = 'DELETE FROM products WHERE id = ?;';
+
+  return connection.execute(query, [id]);
+};
+
 module.exports = {
   getProducts,
+  getProductsById,
+  addProduct,
+  updateProduct,
+  removeProduct,
 };
--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,15 +1,66 @@
 const express = require('express');
-const productCtrl = require('../controllers/product');
+const { 
+  getProducts, 
+  addProduct, 
+  updateProduct, 
+  removeProduct, 
+  getProductsById } = require('../controllers/product');
 
 const router = express.Router();
 
-router.get('/', async (_req, res) => {
+router.get('/', async (_, res) => {
   try {
-    const [result] = await productCtrl.getProducts();
+    const [result] = await getProducts();
     res.status(200).json(result);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: 'Ocorreu um erro ao buscar os produtos' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [result] = await getProductsById(id);
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Ocorreu um erro ao buscar os produtos' });
+  }
+});
+
+router.post('/', async (req, res) => {
+  const product = req.body;
+  try {
+    addProduct(product);
+    res.status(200).json({ message: `${product.name} adionado com sucesso` });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: 'Ocorreu um erro ao adicionar o produto' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const product = req.body;
+  const { id } = req.params;
+  try {
+    updateProduct(product, id);
+    res.status(200).json({ message: `${product.name} atualizado com sucesso` });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Ocorreu um erro ao atualizar o produto' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const product = req.body;
+  const { id } = req.params;
+  try {
+    removeProduct(id);
+    res.status(200).json({ message: `${product.name} removido com sucesso` });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Ocorreu um erro ao remover o produto' });
   }
 });
 
