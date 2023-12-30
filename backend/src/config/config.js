c16f17ec595813ee08a65f6859c06d85aa87a289
MateusDDev <ms4811978@gmail.com>
2023-12-30 20:26:09 -0300

    pausando containers frontent e backend

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,8 +2,8 @@ const config = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
   host: 'localhost',
+  port: process.env.DB_PORT,
   dialect: 'mysql',
 };
 
