73248b65262793a04238bc7f9717c5821a6c7d8e
MateusDDev <ms4811978@gmail.com>
2023-12-30 22:12:26 -0300

    verificação JWT

---
+++ backend/src/auth/validateJWT.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const UserService = require('../services/user.service');
+
+const secret = process.env.JWT_SECRET;
+
+const extractToken = (bearerToken) => bearerToken.spli(' ')[1];
+
+module.exports = async (req, res, next) => {
+  const bearerToken = req.header('Authorization');
+
+  if (!bearerToken) {
+    return res.status(401).json({ error: 'Token não encontrado' });
+  }
+
+  const token = extractToken(bearerToken);
+
+  try {
+    const decoded = jwt.verify(token, secret);
+
+    const user = await UserService.getById(decoded.data.userId);
+
+    if (!user) {
+      return res.status(401).json({ message: 'Erro ao procurar usuário do token.' });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+};
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { getAll, getById, createUser } = require('../controllers/user.controller');
+const validateJWT = require('../auth/validateJWT');
 
 const router = express.Router();
+
+router.post('/', createUser);
+
+router.use(validateJWT);
 
 router.get('/', getAll);
 
 router.get('/:id', getById);
-
-router.post('/', createUser);
 
 module.exports = router;
