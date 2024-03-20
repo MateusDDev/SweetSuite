6d1af81977288ce28c89d59d90704e3c495b6bbb
MateusDDev <ms4811978@gmail.com>
2024-03-20 10:32:07 -0300

    fix: compose and dockerfiles

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,7 +2,7 @@ const config = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  host: 'localhost',
+  host: 'db',
   port: process.env.DB_PORT,
   dialect: 'mysql',
 };
