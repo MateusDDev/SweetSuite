e85ab7879d71a02ec4aaf00f3b80cd72bce67b6d
MateusDDev <ms4811978@gmail.com>
2024-06-19 11:04:21 -0300

    add loading logic

--- frontend/src/components/SearchProduct.tsx
+++ frontend/src/components/SearchProduct.tsx
@@ -5,13 +5,20 @@ import style from './styles/SearchProducts.module.css';
 import MainContext from '../context/MainContext';
 import { requestData } from '../services/request';
 
-function SearchProduct() {
+type SearchProductProps = {
+  setSearchLoading: React.Dispatch<React.SetStateAction<boolean>>
+};
+
+function SearchProduct({ setSearchLoading }: SearchProductProps) {
   const [isFocus, setIsFocus] = useState(false);
   const [query, setQuery] = useState('');
   const { setQueryData } = useContext(MainContext);
 
   const handleSearch = async (q: string) => {
+    if (!q.trim()) return toast.error('Digite um produto para busca');
+
     try {
+      setSearchLoading(true);
       const products = await requestData(`/products/search?name=${q}`);
 
       setQuery('');
@@ -21,9 +28,12 @@ function SearchProduct() {
       }
 
       setQueryData(products);
+      setSearchLoading(false);
     } catch ({ response }: any) {
       console.error(response);
       toast.error(response.data.message);
+    } finally {
+      setSearchLoading(false);
     }
   };
 
