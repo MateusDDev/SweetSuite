ca3a02bae1d729308400874ee0873b7bb99430b5
MateusDDev <ms4811978@gmail.com>
2024-06-19 11:54:55 -0300

    styling loading

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,6 +1,11 @@
+import style from './styles/Loading.module.css';
+import loadingIcon from '../../public/loadingGif.webp';
+
 function Loading() {
   return (
-    <h1>Loading...</h1>
+    <div className={ style.imgContainer }>
+      <img className={ style.img } src={ loadingIcon } alt="loading icon" />
+    </div>
   );
 }
 
